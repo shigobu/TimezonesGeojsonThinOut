2769cca16f3658e1fdf717d26e52304551d1a898
shigenobu <shigenobu314@yahoo.co.jp>
2020-12-20 23:29:49 +0900

    Polygon側作成

---
+++ TimezonesGeojsonThinOut/Feature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimezonesGeojsonThinOut
+{
+	public class Feature
+	{
+		public string Tzid { get; set; } = "";
+
+		public float[][] Coordinates { get; set; }
+	}
+}
--- TimezonesGeojsonThinOut/MainWindow.xaml.cs
+++ TimezonesGeojsonThinOut/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using COMInterfaceWrapper;
 
 namespace TimezonesGeojsonThinOut
 {
@@ -42,6 +42,18 @@ namespace TimezonesGeojsonThinOut
 			}
 		}
 
+		/// <summary>
+		/// 出力選択ボタン選択時のイベント。
+		/// </summary>
+		private void OutSelectButton_Click(object sender, RoutedEventArgs e)
+		{
+			FolderSelectDialog folderSelectDialog = new FolderSelectDialog();
+			if (folderSelectDialog.ShowDialog())
+			{
+				outDirNameTextBox.Text = folderSelectDialog.Path;
+			}
+		}
+
 		private async void ExecuteButton_Click(object sender, RoutedEventArgs e)
 		{
 			ChangeEnable(false);
@@ -51,9 +63,24 @@ namespace TimezonesGeojsonThinOut
 			ChangeEnable(true);
 		}
 
+		/// <summary>
+		/// json間引き処理本体
+		/// </summary>
 		private void Execute()
 		{
-			string jsonFileName = Console.ReadLine();
+			string jsonFileName = GetFileName();
+			if (!File.Exists(jsonFileName))
+			{
+				return;
+			}
+
+			string outDir = GetOutDirectoryName();
+			if (!Directory.Exists(outDir))
+			{
+				return;
+			}
+
+			SetProgressBarIsIndeterminate(true);
 
 			string jsonData = "";
 			using (StreamReader fileStream = new StreamReader(jsonFileName))
@@ -62,32 +89,65 @@ namespace TimezonesGeojsonThinOut
 			}
 			JsonDocument document = JsonDocument.Parse(jsonData);
 
-			Console.WriteLine("読み込み完了");
-
 			JsonElement featuresElement = document.RootElement.GetProperty("features");
-			Console.WriteLine(featuresElement.GetArrayLength());
 			int maxLength = 0;
 			int minLength = int.MaxValue;
 			bool once = false;
 			List<int> countList = new List<int>(500);
-			foreach (var item in featuresElement.EnumerateArray())
+
+			SetProgressBarIsIndeterminate(false);
+
+			SetProgressBarMax(featuresElement.GetArrayLength());
+			for(int j = 0; j < featuresElement.GetArrayLength(); j++)
 			{
-				JsonElement geometry = item.GetProperty("geometry");
+				JsonElement feature = featuresElement[j];
+				JsonElement geometry = feature.GetProperty("geometry");
 				JsonElement coordinates = geometry.GetProperty("coordinates");
 
 				string typeName = geometry.GetProperty("type").GetString();
 				if (typeName == "Polygon")
 				{
-					int temp = coordinates.GetArrayLength();
-					countList.Add(temp);
-					if (temp > maxLength)
+					int pointCount = coordinates[0].GetArrayLength();
+
+					//間引くための係数
+					int numThin;
+					if (pointCount < 10)
 					{
-						maxLength = temp;
+						numThin = 1;
 					}
-					if (temp < minLength)
+					else
 					{
-						minLength = temp;
+						numThin = pointCount / 10;
 					}
+
+					int pointCountThin = pointCount / numThin;
+
+					Feature newFeature = new Feature
+					{
+						Tzid = feature.GetProperty("properties").GetProperty("tzid").GetString(),
+						Coordinates = new float[pointCountThin/* + 1*/][]
+					};
+
+					for (int i = 0; i < pointCount; i += numThin)
+					{
+						//配列外の参照があるので、ガード。
+						if ((i / numThin) >= newFeature.Coordinates.Length)
+						{
+							break;
+						}
+						newFeature.Coordinates[i / numThin] = new float[2];
+						newFeature.Coordinates[i / numThin][0] = RoundFloat(coordinates[0][i][0].GetSingle());
+						newFeature.Coordinates[i / numThin][1] = RoundFloat(coordinates[0][i][1].GetSingle());
+					}
+
+					//ファイル書き出し
+					string jsonString = JsonSerializer.Serialize(newFeature);
+					string newJsonFileName = Path.Combine(GetOutDirectoryName(), newFeature.Tzid.Replace('/', '-') + Path.GetExtension(jsonFileName));
+					using (StreamWriter streamWriter = new StreamWriter(newJsonFileName, false))
+					{
+						streamWriter.Write(jsonString);
+					}
+					//break;
 				}
 				else if (typeName == "MultiPolygon")
 				{
@@ -118,7 +178,7 @@ namespace TimezonesGeojsonThinOut
 				{
 					/*何もしない*/
 				}
-
+				SetProgressBarValue(j);
 			}
 		}
 
@@ -131,6 +191,8 @@ namespace TimezonesGeojsonThinOut
 			selectButton.IsEnabled = enable;
 			fileNameTextBox.IsEnabled = enable;
 			executeButton.IsEnabled = enable;
+			outDirNameTextBox.IsEnabled = enable;
+			outSelectButton.IsEnabled = enable;
 		}
 
 		delegate void SetProgressBarMaxDelegate(int max);
@@ -142,11 +204,11 @@ namespace TimezonesGeojsonThinOut
 		{
 			if (progress.Dispatcher.CheckAccess())
 			{
-				progress.Dispatcher.Invoke(new SetProgressBarMaxDelegate(SetProgressBarMax), max);
+				progress.Maximum = max;
 			}
 			else
 			{
-				progress.Maximum = max;
+				progress.Dispatcher.Invoke(new SetProgressBarMaxDelegate(SetProgressBarMax), max);
 			}
 		}
 
@@ -159,12 +221,70 @@ namespace TimezonesGeojsonThinOut
 		{
 			if (progress.Dispatcher.CheckAccess())
 			{
-				progress.Dispatcher.Invoke(new SetProgressBarValueDelegate(SetProgressBarMax), val);
+				progress.Value = val;
 			}
 			else
 			{
-				progress.Value = val;
+				progress.Dispatcher.Invoke(new SetProgressBarValueDelegate(SetProgressBarValue), val);
 			}
+		}
+
+		delegate void SetProgressBarIsIndeterminateDelegate(bool isIndeterminate);
+		/// <summary>
+		/// プログレスバーの値無し状態の設定をします。
+		/// </summary>
+		/// <param name="isIndeterminate"></param>
+		private void SetProgressBarIsIndeterminate(bool isIndeterminate)
+		{
+			if (progress.Dispatcher.CheckAccess())
+			{
+				progress.IsIndeterminate = isIndeterminate;
+			}
+			else
+			{
+				progress.Dispatcher.Invoke(new SetProgressBarIsIndeterminateDelegate(SetProgressBarIsIndeterminate), isIndeterminate);
+			}
+		}
+
+		/// <summary>
+		/// ファイル名テキストボックスの内容を取得します。
+		/// </summary>
+		/// <returns>ファイル名</returns>
+		private string GetFileName()
+		{
+			if (fileNameTextBox.Dispatcher.CheckAccess())
+			{
+				return fileNameTextBox.Text;
+			}
+			else
+			{
+				return fileNameTextBox.Dispatcher.Invoke(GetFileName);
+			}
+		}
+
+		/// <summary>
+		/// 出力先名を取得します。
+		/// </summary>
+		/// <returns>フォルダ名</returns>
+		private string GetOutDirectoryName()
+		{
+			if (outDirNameTextBox.Dispatcher.CheckAccess())
+			{
+				return outDirNameTextBox.Text;
+			}
+			else
+			{
+				return outDirNameTextBox.Dispatcher.Invoke(GetOutDirectoryName);
+			}
+		}
+
+		private float RoundFloat(float val)
+		{
+			//整数部分の桁数を計算
+			int num = Math.Abs((int)Math.Truncate(val));
+			int digit = (num == 0) ? 0 : ((int)Math.Log10(num) + 1);
+
+			return (float)Math.Round(val, 6 - digit);
 		}
 	}
 }
